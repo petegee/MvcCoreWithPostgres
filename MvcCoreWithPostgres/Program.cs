f8bb69b623e1a5841ed1945d1ff85fec1212a5e7
Peter Glassey [DATACOM] <PeterGla@datacom.co.nz>
2017-07-17 12:22:35 +1200

    Add project files.

---
+++ MvcCoreWithPostgres/Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using System.IO;
+
+namespace MvcCoreWithPostgres
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseStartup<Startup>()
+                .UseApplicationInsights()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
